ff3835a9174c7fe1bea7f1ce2fef652f4bf6b874
Bikash <bikash11roy@gmail.com>
2017-03-26 23:50:00 +0600

    Database Connection Access files created

---
+++ Web Lab/ProjectsCV/include/database.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bikash
+ * Date: 3/26/2017
+ * Time: 11:37 PM
+ */
+
+require_once ('config.php');
+
+class MySQLDatabase{
+    private $connection;
+
+    function __construct()
+    {
+    }
+
+    public function open_connection(){
+        $this->connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+        if(mysqli_errno($this->connection)){
+            die("Database connection failed: " .
+                mysqli_connect_error().
+                "(" . mysqli_connect_errno(). ")"
+            );
+        }
+    }
+}
