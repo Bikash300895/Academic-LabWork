9a51aa5bb768b75376aba9c9cf7a0c2923f121c3
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-19 19:34:22 +0600

    removing html
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- Database Lab/Lab4/Understanding Primary Key(PK) INDEX Constraint in Oracle_files/ca-pub-2652062086099003.js
+++
@@ -1,1 +0,0 @@
-(google_async_config = window.google_async_config || {})['ca-pub-2652062086099003'] = {"sra_enabled":false};
--- Web Lab/Lab03 - PHP basics/php_pages/new.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<?php    
- echo "Hello World<br>";
-		print "Hello World kuet";
-		//This is a comment		/*		This is a comment		block		*/ 
-?>
-</body>
-</html>
