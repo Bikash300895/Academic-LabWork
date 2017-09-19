4774db7555d48226c87016881a779757c736b5a1
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-19 16:58:10 +0600

    LL1 parsing logic implemented

--- Compiler_Design/Projects/LL1 Parser/main.py
+++ Compiler_Design/Projects/LL1 Parser/main.py
@@ -3,15 +3,33 @@ parse_table = {
         "a" : "aBa"
     },
     "B" : {
-        "a" : None,
+        "a" : "",
         "b" : "bB"
     }
 }
 
 stack = "$S"
-word = "abba"
+word = input()
+word = word + "$"
 
-while len(stack) >0:
+while len(stack) > 0:
     stackTop = stack[len(stack) - 1]
     inputFront = word[0]
+    
+    if stackTop in parse_table:
+        stack = stack[:-1]
+        production = parse_table[stackTop][inputFront]
+        reversedProduction = production[::-1]
+        stack = stack + reversedProduction
+        
+    elif stackTop == inputFront:
+        if stackTop == "$":
+            print("Successfully compiled")
+            break
+        
+        stack = stack[:-1]
+        word = word[1:]
+    else:
+        print("Error in compiling")
+        break
 
